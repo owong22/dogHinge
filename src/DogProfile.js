4d0550200d16c557cd0dd05af5c96920a20f060f
owong22 <itsoscarwong@gmail.com>
2023-01-25 20:37:43 -0800

    Stopping point for the day

--- src/DogProfile.js
+++ src/DogProfile.js
@@ -5,6 +5,7 @@ const DogProfile = ({ dogProf }) => {
       <p>{dogProf.info.results[0].name.first}</p>
       <p>{dogProf.info.results[0].gender}</p>
       <p>{Math.floor(dogProf.info.results[0].dob.age / 7)} years</p>
+      <p>{dogProf.info.results[0].location.country}</p>
     </div>
   );
 };
